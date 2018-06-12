3260fab1a86373ff2ca297c67c9840d5ed1289db
kajtazovai <kajtazova@gmail.com>
2018-06-12 10:54:18 +0200

    Add User

---
+++ MusicGame/MusicGame/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicGame
+{
+    public class User
+    {
+        public int Username { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+
+
+
+    }
+}
