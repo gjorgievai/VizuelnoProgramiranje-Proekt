f842d1da435d32b181135f132ec368ff3b8bae46
kajtazovai <kajtazova@gmail.com>
2018-06-13 15:22:13 +0200

    Change for songs

--- MusicGame/MusicGame/Form1.cs
+++ MusicGame/MusicGame/Form1.cs
@@ -15,23 +15,23 @@ namespace MusicGame
 {
     public partial class Form1 : Form
     {
-        public User user;
+        public Song song;
         public SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\MusicData.mdf;Integrated Security=True");
         public SqlCommand command = new SqlCommand();
         DataSet dataSet = new DataSet();
         SqlDataAdapter adapter = new SqlDataAdapter();
-        List<User> users { get; set; }
+        List<Song> songs { get; set; }
           
         public Form1()
         {
             InitializeComponent();
-            users = new List<User>();
+            songs = new List<Song>();
            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            adapter.SelectCommand = new SqlCommand("SELECT * FROM [User]",connection);
+            adapter.SelectCommand = new SqlCommand("SELECT * FROM [Song]",connection);
             adapter.Fill(dataSet);
             datagrid.DataSource = dataSet.Tables[0];
             for (int i = 0; i < datagrid.Rows.Count-1; i++)
@@ -39,13 +39,15 @@ namespace MusicGame
                 
 
                     int id = Int32.Parse(datagrid.Rows[i].Cells[0].Value.ToString());
-                    string username = datagrid.Rows[i].Cells[1].Value.ToString();
-                    int score = Int32.Parse(datagrid.Rows[i].Cells[2].Value.ToString());
-                    user = new User(id, username, score);
-                    users.Add(user);
+                    string name = datagrid.Rows[i].Cells[1].Value.ToString();
+                    string artist = datagrid.Rows[i].Cells[2].Value.ToString();
+                    int year = Int32.Parse(datagrid.Rows[i].Cells[3].Value.ToString());
+                    song = new Song(id, name, year, artist);
+                    lbSongs.Items.Add(song.NameSong);
+                    
                 
             }
-            lblUserName.Text = users[0].UserName;
+            
         }
     }
 }
